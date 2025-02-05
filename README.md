bca473d017d25b76e8a642f3b45c6701ba1815a8
omarburnett <omargithub5545@gmail.com>
2025-02-05 15:26:27 +0000

    cat5s

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# coding
+    input('hello)
---
+++ forloop.py
@@ -0,0 +1,13 @@
+#a forloop is aq lopp that scansw a list
+
+#fruits = ["apple", "banana", "cherry"]
+#for x in fruits:
+    #print(x) 
+
+  # for loop only goes through a loop once 
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+    if x == 'banna':
+        thislist.remove("banana"):
+    return thislist
+

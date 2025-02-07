9ab0cbeab3e0c970950b6676ad5341d5ff13dcb5
omarburnett <omargithub5545@gmail.com>
2025-02-07 15:24:43 +0000

    Activity February 7th, 2025

---
+++ 2/7/asyncActivityFeb7.py
@@ -0,0 +1,33 @@
+
+#Async Activity February 7th, 2025
+#Please create a new file called asyncActivityFeb7.py or asyncActivityFeb7.js and complete the following questions.
+
+#This assignment is due at the end of class. If you are not present for class, this assignment will need to be completed as homework before 11pm tonight (Feb. 7, 2025).
+
+#IMPORTANT
+#Once you've completed the assignment, please do the following steps to ensure your assignment is submitted properly.
+
+#Commit and sync your work to your github repository. - DO NOT PUT THE LINK TO YOU CODESPACES HERE- IT MOST BE YOUR REPOSITIORY.
+#Drop the link to your repository in the following link below
+#Async Activity Feb.7, 2025 Submission Link
+#These questions are code agnostic (meaning it does not matter which coding language you are learning, you should be able to answer them. )
+#In your own words, describe the difference between a "FOR" loop and a "WHILE" loop. Please make sure your response is written in a complete sentence.
+
+#Describe a situation where you would use a WHILE LOOP in a computer program. Then, attempt to create your example in code.
+
+#You have been hired by temple university to work on a graduation checker program. your program should be able to find the highest grade in a list.
+#  Create a function that will loop through each number in the list and identify if it is the highest number. When your program finds the highest grade,
+#  it should return a message telling the user what the highest grade is.
+
+# a while loop should bwe use in a situation where a process is needed to be repeated for an repetitive process 
+
+def schoolgrades():
+   input('hello what grades are you looking for')
+   grades=[1,2,4,75]
+   for number in grades:
+        if grades>4:
+            print(75)
+i = 1
+while i < 6:
+    print(schoolgrades)
+    i += 1

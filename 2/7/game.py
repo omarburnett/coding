f10fffc3a7aa63e62066da6b0740cbb579121701
omarburnett <omargithub5545@gmail.com>
2025-02-13 15:28:38 +0000

    game2/13

---
+++ 2/7/game.py
@@ -0,0 +1,14 @@
+#def my_function(1,2):
+  #  1='bob'
+ #   2='cheese'
+#hi == 1
+
+#parmeters are = arguments 
+def kitty():
+    cats={'red','blue','der'}
+    for x in cats :
+        len(cats)
+        return( len(cats))
+kitty()
+print(kitty)
+

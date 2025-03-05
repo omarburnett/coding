b1266f6f8fcbe6447a6c56b235eb7dd8158d7b30
omarburnett <omargithub5545@gmail.com>
2025-03-05 17:52:29 +0000

    not done

---
+++ 2/7/test.py
@@ -0,0 +1,5 @@
+input('do you dare to challenge me ')
+if 'yes':
+    print('so be it')
+elif  'yes'
+    print ('[coward]')
---
+++ Async Activity February 21nd, 2025.py
@@ -0,0 +1,35 @@
+#Async Activity February 21nd, 2025
+#Please create a new file called asyncActivityFeb21.py or asyncActivityFeb21.js and complete the following questions.
+#IMPORTANT Once you've completed the assignment, please do the following steps to ensure your assignment is submitted properly.
+
+#Commit and sync your work to your github repository. - DO NOT PUT THE LINK TO YOU CODESPACES! - IT MOST BE YOUR REPOSITIORY. Drop the link to your repository in the following link below
+
+#Feb21 Async Repo Submission form
+
+#This assignment is due at the end of class. If you are not present for class, this assignment will need to be completed as homework before 11pm tonight (Feb. 21, 2025).
+
+#1. Please watch this video on Game Design Documents
+#Game Design Document
+
+#2. On your document answer the following questions about your upcoming game project
+#As we embark on our game development journey, I would like you to begin brainstorming what kind of game you want to build by creating a draft of a game design document. Your game can be a custom and unique project or it can be clone of an already existing game.
+
+#What is the name of your game?
+
+#What genre is your game going to be in (horror, action-adventure, shooting, fighting, platformer, etc.)
+
+#What is the objective and general game loop of your game? Please write your response in complete sentences. example: players will try to score points by hitting randomized targets in a set amount of time.
+
+#What already existing games does your game take inspiration from? Please provide at least 3 examples, including the name of the game, and describe the aspect your inspired by in complete sentences.
+
+#Please describe the mechanics, or functional parts that will be in your game example:
+
+#a. my game will allow the player to move across the screen horizontally
+#b. my game will feature different weapon types that will have different effects.
+
+'game name: block dash'
+'action'
+'collect blocks/items and power up . some encouters may require past and present dynamic skills to pass the levle,if the character fails they will be met with an harder levle instead of restarting'
+'my inspiration stems from good of war ,mario and injustice 2 . injustice 2 uses dynamic power ups to compliment past knowledge'
+'mario provides power ups for the player and god of war , dynamic enemies'
+
